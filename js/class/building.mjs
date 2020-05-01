ef3efe376616d4c788b860b1a579de0024b64c95
Lorris <Lorris.dhedin@hotmail.fr>
2020-05-01 17:07:49 +0200

    Js init Ok

--- js/class/building.mjs
+++ js/class/building.mjs
@@ -1,8 +1,8 @@
 export class Bulding {
-        constructor(name,description,number,cookiesPerSecond,cost){
+        constructor({name,description,number,cookiesPerSecond,cost}){
             this._name = name;
             this._description = description;
-            this._number = number;
+            this._number = number || 0;
             this._cookiesPerSecond = cookiesPerSecond;
             this._cost = cost;
         }
--- js/main.mjs
+++ js/main.mjs
@@ -1,5 +1,66 @@
 import { Bakery } from "./class/bakery.mjs";
 
-const test = new Bakery;
+
+const myBakery = new Bakery(),
+get_bakery_h2 = document.querySelector('h2'),
+get_bakery_span1 = document.querySelector('span'),
+get_bakery_span2 = document.querySelectorAll('span');
+
+
+
+get_bakery_h2.innerHTML = `${myBakery._name}'s Bakery`; 
+get_bakery_span1.innerHTML = `${myBakery._cookiesPerClick}` ;
+get_bakery_span2[1].innerHTML = `${myBakery._cookiesPerSecond}`;
+
+
+
+// let test = document.getElementById('game').innerHTML
+
+// console.log(test);
+// test = ' <div id="building-cursor" class="locked disabled">
+// <div class="icon"></div>
+// <div class="name">Cursor</div>
+// <div class="cost">15</div>
+// <div class="number"></div>
+// </div> '
+test = document.getElementById('game').innerHTML
+test = document.createElement('div');
+test.innerHTML = ' <div id="building-cursor" class="locked disabled">
+<div class="icon"></div>
+<div class="name">Cursor</div>
+<div class="cost">15</div>
+<div class="number"></div>
+</div>'
 
 console.log(test);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
